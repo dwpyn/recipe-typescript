db11f4f50a2bdc1e69e6c766b6ac9af87d54bc4f
dwpyn <dwpyndev@gmail.com>
2023-01-05 07:38:23 +0800

    typescript solved

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -3,12 +3,16 @@ import { Link } from 'react-router-dom'
 // styles
 import './RecipeList.css'
 
-interface Recipes {
-  id: number,
-
+interface Recipe {
+  "id": string,
+  "title": string,
+  "ingredients": string[],
+  "method": string,
+  "cookingTime": string,
 }
 
-export default function RecipeList({ id }): React.FC<Recipes> {
+
+export default function RecipeList({ recipes }: { recipes: Recipe[] }) {
 
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>
